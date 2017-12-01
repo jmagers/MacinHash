f3ec6be793d4757ed04188b64c6e0f64d2193139
Jake Magers <jmagers12@gmail.com>
2017-11-30 22:16:22 -0800

    Use argparse

--- machash.py
+++ machash.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
 
+import argparse
 import plistlib
 import sys
 
-# Check number of args
-if not 2 <= len(sys.argv) <= 3:
-    sys.exit("Must provide 1 or 2 arguments!")
+# Arguments
+parser = argparse.ArgumentParser(description='Convert Mac plist password file '
+                                 'to hash file')
+parser.add_argument('plistfile',
+                    type=argparse.FileType('rb'),
+                    help='plist file to convert to hash')
+parser.add_argument('hashfile',
+                    type=argparse.FileType('w'),
+                    nargs='?',
+                    default='-',
+                    help='file to store resulting hash')
+args = parser.parse_args()
 
 # Load plist file
-PLIST_FILE_NAME = sys.argv[1]
-plist = None
 try:
-    with open(PLIST_FILE_NAME, 'rb') as pfile:
-        plist = plistlib.load(pfile)
-except:
-    sys.exit("Could not load plist file!")
-
-# Set file to store results
-try:
-    RESULT_FILE = open(sys.argv[2], 'w')
-except IndexError:
-    RESULT_FILE = sys.stdout
-except:
-    sys.exit("Could not open results file!")
+    plist = plistlib.load(args.plistfile)
+except plistlib.InvalidFileException:
+    sys.exit("Could not parse plist file!")
 
 # Collect hash data
 shadow_hash_data = plistlib.loads(plist['ShadowHashData'][0])
@@ -33,7 +32,4 @@ entropy = data['entropy'].hex()
 
 # Format and output hash data
 formatted_hash = '$'.join(['$ml', iterations, salt, entropy])
-print(formatted_hash, file=RESULT_FILE)
-
-# Cleanup
-RESULT_FILE.close()
+print(formatted_hash, file=args.hashfile)
